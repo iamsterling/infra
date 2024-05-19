0e17a2e627cda1b6ca623a67bc4550c88f7a0a94
Sterling <jsterlingholt@me.com>
2024-05-18 19:20:37 -0500

    testing fargate

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "network" {
-  source = "./."
+  source = "./"
   vpc_id = module.network.vpc_id
   public_subnet_id = module.network.public_subnet_id
   private_subnet_id = module.network.private_subnet_id
